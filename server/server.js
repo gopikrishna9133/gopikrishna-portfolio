84929a2ed2906a002ac8e43036001e4abb7308d2
gopikrishna9133 <krishkrishna9133@gmail.com>
2026-02-12 20:35:27 -0600

    update vercel link in server

--- server/server.js
+++ server/server.js
@@ -16,7 +16,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://gopikrishna-portfolio-sandy.vercel.app/"
+    ]
+  }));
 app.use(express.json());
 
 app.use("/api/projects", projectRoutes);
